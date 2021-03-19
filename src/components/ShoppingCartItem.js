150a2398885d00ec796380f69dfa85c1968aa634
Adam Purdy <purdyrad@Adams-MacBook-Air.local>
2021-03-18 21:29:28 -0400

    done

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,18 +1,18 @@
-import React, {useContext} from 'react';
-import {CartContext} from '../contextors/ProductContext';
+import React from 'react';
+import {ProductContext} from '../contextors/ProductContext';
 
 
 
-const Item = () => {
-	const crt = useContext(CartContext);
+const Item = props => {
+	
 	return (
 		<div className="shopping-cart_item">
-			<img src={crt.image} alt={`${crt.title} book`} />
+			<img src={props.image} alt={`${props.title} book`} />
 
 
 			<div>
-				<h1>{crt.title}</h1>
-				<p>$ {crt.price}</p>
+				<h1>{props.title}</h1>
+				<p>$ {props.price}</p>
 				<button>Remove from cart</button>
 			</div>
 		</div>
